34cae6bc24c64150f729bb14c5899bc53737dede
SadeteDragaj <81782815+SadeteD@users.noreply.github.com>
2023-10-16 16:24:06 +0200

    Actors data added

---
+++ Data/Services/IActorsService.cs
@@ -0,0 +1,16 @@
+﻿using Cinemava.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cinemava.Data.Services
+{
+    public interface IActorsService
+    {
+        Task<IEnumerable<Actor>> GetAllAsync();
+
+       Task<Actor>GetByIdAsync(int id);
+        Task AddAsync(Actor actor);
+        Task<Actor> UpdateAsync(int id, Actor newActor);
+        Task DeleteAsync(int id); 
+    }
+}
--- Models/Actor.cs
+++ Models/Actor.cs
@@ -8,10 +8,13 @@ namespace Cinemava.Models
         [Key]
         public int Id { get; set; }
         [Display(Name = "Profile Picure")]
+        [Required(ErrorMessage ="Profile Picture is required")]
         public string ProfilePictureURL { get; set; }
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is required")]
         public string FullName { get; set; }
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is required")]
         public string Bio { get; set; }
 
         public List<Actors_Movie> Actor_Movies { get; set; }
